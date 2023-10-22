e2b53ea5b7fd9397c2cf78df2eaf02f4690783be
Oleg Golota <oo449@bk.ru>
2023-10-23 00:12:47 +0300

    Появилась функция вывода пустого поля

--- Program.cs
+++ Program.cs
@@ -6,11 +6,24 @@
         static char[] exampleWord = {'a', 'b', 'c', 'd', 'e'};
 
         // Поле
-        static char[,] matrix = new char[6, 5]; 
+        static char[,] field = new char[6, 5]; 
 
         static void Main(string[] args)
         {
-            
+            makeField();
+        }
+
+        static void makeField()
+        {
+            for(int i = 0; i < field.GetLength(0); i++)
+            {
+                for(int j = 0; j < field.GetLength(1); j++)
+                {
+                    field[i,j] = '*';
+                    System.Console.Write(field[i,j] + " ");
+                }
+            System.Console.WriteLine();
+            }
         }
 
         
